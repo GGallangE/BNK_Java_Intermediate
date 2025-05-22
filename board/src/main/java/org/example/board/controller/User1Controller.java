54e32bc5d74c14a67b9a3e82b1a220dc4cd56a6e
GGallangE <ehrbs4789@gmail.com>
2025-05-22 09:28:29 +0900

    Add: Spring day02

--- board/src/main/java/org/example/board/controller/User1Controller.java
+++ board/src/main/java/org/example/board/controller/User1Controller.java
@@ -4,8 +4,11 @@ import org.example.board.dto.User1DTO;
 import org.example.board.service.User1Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class User1Controller {
@@ -20,31 +23,39 @@ public class User1Controller {
 
     @PostMapping("/user1/register")
     public String register(User1DTO user1DTO){
-
         System.out.println(user1DTO);
 
         user1Service.insertUser1(user1DTO);
 
-        return "/user1/register";
+        // 리다이렉트
+        return "redirect:/user1/list";
     }
 
     @GetMapping("/user1/list")
-    public String list(){
+    public String list(Model model){
+        // 목록 요청
+        List<User1DTO> user1List = user1Service.selectAllUser1();
+
+        // 모델 참조 - View(Html)에서 Controller 데이터를 참조하기 위한 처리
+        model.addAttribute("user1List", user1List);
+
+        // 페이지 출력(포워드)
         return "/user1/list";
     }
 
+
+
+
+
+
     @GetMapping("/user1/modify")
-    public String modify(){
+    public String modify(int no, Model model){ // no 파라미터 수신
+        System.out.println("no : " + no);
+
+        User1DTO user1DTO = user1Service.selectUser1(no);
+        model.addAttribute(user1DTO);
+
         return "/user1/modify";
     }
 
-    @PostMapping("/user1/modify")
-    public String modify(User1DTO user1DTO){
-
-        System.out.println(user1DTO);
-
-        user1Service.insertUser1(user1DTO);
-
-        return "/user1/register";
-    }
 }
--- board/src/main/java/org/example/board/dao/User1DAO.java
+++ board/src/main/java/org/example/board/dao/User1DAO.java
@@ -3,17 +3,125 @@ package org.example.board.dao;
 import org.example.board.dto.User1DTO;
 import org.springframework.stereotype.Repository;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class User1DAO {
 
-    public void insertUser1(User1DTO user1DTO) {}
+    private String host = "jdbc:h2:tcp://localhost/~/test";
+    private String user = "sa";
+    private String pass = "1234";
 
-    public void selectUser1(){}
+    public void insertUser1(User1DTO user1DTO) {
+        try{
+            // 데이터베이스 접속
+            Connection conn = DriverManager.getConnection(host, user, pass);
 
-    public void selectAllUser1(){}
+            // SQL 객체 생성 및 데이터 매핑
+            String sql = "INSERT INTO user1 (name, birth, age, addr) VALUES (?,?,?,?)";
+            PreparedStatement psmt = conn.prepareStatement(sql);
+            psmt.setString(1, user1DTO.getName());
+            psmt.setString(2, user1DTO.getBirth());
+            psmt.setInt(3, user1DTO.getAge());
+            psmt.setString(4, user1DTO.getAddr());
 
-    public void updateUser1(){}
+            // SQL 실행
+            psmt.executeUpdate();
+
+            // 자원해제
+            psmt.close();
+
+            // 데이터베이스 종료
+            conn.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public User1DTO selectUser1(int no){
+
+        // 반환용 DTO 선언
+        User1DTO user1DTO = null;
+
+        try{
+            Connection conn = DriverManager.getConnection(host, user, pass);
+
+            String sql = "select * from user1 where no = ?";
+            PreparedStatement psmt = conn.prepareStatement(sql);
+            psmt.setInt(1, no);
+
+            ResultSet rs = psmt.executeQuery();
+
+            if (rs.next()){
+                // userDTO 생성
+                user1DTO = new User1DTO();
+                user1DTO.setNo(rs.getInt(1));
+                user1DTO.setName(rs.getString(2));
+                user1DTO.setBirth(rs.getString(3));
+                user1DTO.setAge(rs.getInt(4));
+                user1DTO.setAddr(rs.getString(5));
+            }
+
+            rs.close();
+            psmt.close();
+            conn.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return user1DTO;
+    }
+
+    public List<User1DTO> selectAllUser1(){
+
+        // 반환용 결과 리스트
+        List<User1DTO> user1DTOS = new ArrayList<>();
+
+        try {
+            Connection conn = DriverManager.getConnection(host, user, pass);
+
+            String sql = "SELECT * FROM user1";
+            Statement stmt = conn.createStatement();
+
+            // SQL 실행 후 결과셋 처리
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while(rs.next()){ // 커서를 한행씩 내리면서 반복
+                // 객체생성
+                User1DTO user = new User1DTO();
+                user.setNo(rs.getInt(1));
+                user.setName(rs.getString(2));
+                user.setBirth(rs.getString(3));
+                user.setAge(rs.getInt(4));
+                user.setAddr(rs.getString(5));
+
+                // 리스트 저장
+                user1DTOS.add(user);
+            }
+
+            // 자원해제
+            rs.close();
+            stmt.close();
+
+            // 데이터베이스 종료
+            conn.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return user1DTOS;
+    }
+
+    public void updateUser1(){
+
+        //UPDATE user1 SET name=?, birth=?, age=?, addr=? WHERE no = ?;
+    }
 
     public void deleteUser1(){}
+
 
 }
